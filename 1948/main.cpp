7a128cae52d42669cd08c2706c9b7546736704a1
BROWN1213 <s091508@gamil.com>
2024-02-07 23:45:34 +0900

    1948

--- 1948/main.cpp
+++ 1948/main.cpp
@@ -3,9 +3,17 @@
 using namespace std;
 
 int main(){
-
+    int N, M;
+    scanf("%d", &N);
+    scanf("%d", &M);
+    int input[3];
+    for(int i = 0; i< M; i++){
+        scanf("%d %d %d", input, input+1, input+2);
+        //printf("%d %d %d\n", input[0], input[1], input[2]);
+        
+    }
+    int start, end;
+    scanf("%d %d", &start, &end);
     
-
-
     return 0;
 }
